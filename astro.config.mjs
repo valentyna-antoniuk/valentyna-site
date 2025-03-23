c387e1f4e32bd58b72c50d708350297753433e0a
antoniukvalentyna <antoniuk.valentyna@gmail.com>
2025-03-23 17:50:56 +0200

    different changes: react, mdx, basic markup for all parts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   experimental: {
     svg: true,
   },
+  integrations: [mdx(), react()],
 });
---
+++ src/features/path/steps.ts
@@ -0,0 +1,14 @@
+export const careerJourney = [
+  "Passion for Computer Science and participation in School Olympiads",
+  "Enrolled in Computer Engineering Faculty",
+  "Bachelor's Degree with Honors",
+  "Master's Degree with Honors",
+  "Realized my passion for Web Development",
+  "First projects and internships, gaining hands-on experience in coding",
+  "Front-End Development: years of experience focusing on markup, layout, and user interfaces",
+  "Realized Front-End is more than just layout – it’s about user experience",
+  "Learning JavaScript, React, and modern development tools, building a solid technical foundation",
+  "React: Over the years, developed multiple products, focusing on performance and scalability",
+  "Front-End Team Lead, managing projects and driving technical decisions",
+  "Gained leadership experience, now leading teams and mentoring junior developers to success",
+];
