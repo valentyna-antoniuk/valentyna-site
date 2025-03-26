996877158e87b8fed55ff4ba094bbe67286bef66
antoniukvalentyna <antoniuk.valentyna@gmail.com>
2025-03-26 21:35:56 +0200

    overview of career

--- src/features/path/steps.tsx
+++ src/features/path/steps.tsx
@@ -1,13 +1,37 @@
 export const careerJourney = [
-  { text: '<span>Passion for computers and participation in School Olympiads</span>', emoji: "🏫" },
-  { text: '<span>Enrolled in <b>Computer Engineering Faculty</b></span>', emoji: "👩‍🎓" },
+  {
+    text: "<span>Passion for computers and participation in School Olympiads</span>",
+    emoji: "🏫",
+  },
+  {
+    text: "<span>Enrolled in <b>Computer Engineering Faculty</b></span>",
+    emoji: "👩‍🎓",
+  },
   { text: "<span><b>Bachelor's Degree</b> with Honors</span>", emoji: "🎓" },
   { text: "<span><b>Master's Degree</b> with Honors</span>", emoji: "🎓" },
   { text: "Realized my passion for Web Development", emoji: "💡" },
-  { text: "First projects and internships, gaining hands-on experience in coding", emoji: "👩‍💻" },
-  { text: "<span><b>Front-End Development</b>: years of experience focusing on markup, layout, and user interfaces</span>", emoji: "🖥️" },
-  { text: "Learning JavaScript, React, and modern development tools, building a solid technical foundation", emoji: "👩‍🏫️" },
-  { text: "<span><b>React</b>: Over the years, developed multiple products, focusing on performance and scalability</span>", emoji: "⚡" },
-  { text: "<span><b>Front-End Team Lead</b>: managing projects and driving technical decisions</span>", emoji: "🚀" },
-  { text: "<span><b>Leadership Experience</b>: leading teams and mentoring developers to success</span>", emoji: "🌟" },
+  {
+    text: "First projects and internships, gaining hands-on experience in coding",
+    emoji: "👩‍💻",
+  },
+  {
+    text: "<span><b>Front-End Development</b>: years of experience focusing on markup, layout, and user interfaces</span>",
+    emoji: "🖥️",
+  },
+  {
+    text: "Learning JavaScript, React, and modern development tools, building a solid technical foundation",
+    emoji: "👩‍🏫️",
+  },
+  {
+    text: "<span><b>React</b>: Over the years, developed multiple products, focusing on performance and scalability</span>",
+    emoji: "⚡",
+  },
+  {
+    text: "<span><b>Front-End Team Lead</b>: managing projects and driving technical decisions</span>",
+    emoji: "🚀",
+  },
+  {
+    text: "<span><b>Leadership Experience</b>: leading teams and mentoring developers to success</span>",
+    emoji: "🌟",
+  },
 ];
